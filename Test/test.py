8d85b39ca5d3144d29b177750cf81f495337497a
Jung217 <alex24922665@gmail.com>
2022-12-20 21:22:36 +0800

    Fix HBD reply

---
+++ Test/test.py
@@ -0,0 +1,4 @@
+import random
+
+bless = ['生日快樂！希望你的所有願望都能成真','準備好開始倒數了嗎？下次跟你說生日快樂是 365 天之後，生日快樂！','原本想送你一個最可愛的禮物，後來只能找第二可愛的，因為你排名第一呀。','大壽星，小蛋糕，絕配。生日快樂！','你生日的這一天，我沒有跟你在一起，只希望你能快樂、健康、美麗，生命需要奮鬥、創造和把握！生日快樂！','Happy birthday to the most wonderful friend in my heart.','Wish you a happy birthday! May the best and the loving things be some of the joy your birthday bring.']
+print(bless[random.randint(0, len(bless)-1)])
