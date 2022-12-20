7b5f3b5da16b6492eaa5038eaabad6b13099b8b7
Jung217 <alex24922665@gmail.com>
2022-12-20 20:51:57 +0800

    fix heroku test

--- app.py
+++ app.py
@@ -24,3 +24,13 @@ def callback():
         abort(400)
  
     return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
