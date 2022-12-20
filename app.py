42fb2b57cfe9d7c83284990a4d9700587a2a1ea4
Jung217 <alex24922665@gmail.com>
2022-12-20 22:04:29 +0800

    Add HBD sticker v3

--- app.py
+++ app.py
@@ -37,8 +37,11 @@ def handle_message(event):
             package_id='6325',
             sticker_id='10979924'
         )
+        a = 0
         line_bot_api.reply_message(event.reply_token, sticker_message)
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(bless[random.randint(0, len(bless)-1)]))
+        a = 1
+        if a == 1 :
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(bless[random.randint(0, len(bless)-1)]))
     else:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
 
