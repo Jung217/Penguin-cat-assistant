cf6d4e2d310c0f20bfbe1c32f49df9a6c92f6e28
Jung217 <alex24922665@gmail.com>
2022-12-20 23:36:27 +0800

    Add Map NQUv7

--- Test/test.py
+++ Test/test.py
@@ -6,3 +6,13 @@
             longitude= 118.32249208222159
             )
 l       line_bot_api.reply_message(event.reply_token, location_message)
+
+elif re.match("金門大學在哪",message):
+        location_message = {
+            "type": "location",
+            "title": "國立金門大學",
+            "address": "892金門縣金寧鄉大學路1號",
+            "latitude": 24.44829638687612,
+            "longitude": 118.32249208222159
+        }
+        line_bot_api.reply_message(event.reply_token, sticker_message)
