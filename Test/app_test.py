ed38d8e6cc11d172f41f410c3b41b1bbc834e7c6
Jung217 <alex24922665@gmail.com>
2022-12-20 20:45:15 +0800

    heroku test

--- Test/app_test.py
+++ Test/app_test.py
@@ -27,7 +27,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
 
-if __name__ == '__main__':
-    app.run()
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
