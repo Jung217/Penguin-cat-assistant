a906b58579d9717ecdbbeac7c5c1d903451c2674
Jung217 <alex24922665@gmail.com>
2022-12-20 23:42:25 +0800

    Add Map NQUv9

--- app.py
+++ app.py
@@ -41,13 +41,12 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, sticker_message)
     elif re.match("金門大學在哪",message):
-        location_message = {
-            type: "location",
-            title: "國立金門大學",
-            address: "892金門縣金寧鄉大學路1號",
-            latitude: 24.44829638687612,
-            longitude: 118.32249208222159
-        }
+        location_message = LocationSendMessage(
+            title= "國立金門大學", 
+            address= "892金門縣金寧鄉大學路1號",
+            latitude= 24.44829638687612,  
+            longitude= 118.32249208222159
+        )
         line_bot_api.reply_message(event.reply_token, location_message)
     else:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
