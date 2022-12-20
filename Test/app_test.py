7cf33e1c007463334526739ea51c6f5a6e931712
Jung217 <alex24922665@gmail.com>
2022-12-20 16:14:51 +0800

    fix test/a_t.py

--- Test/app_test.py
+++ Test/app_test.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('8mHVNSHnlj3xx9180Kt+XKh6oVyljAhhV/qOrXL2XXorpdwIO5eard7Jfkvd2wR8P+cEQdUJQ3sEcI0clytSMsoaMH7fQZt4zjHoOUMdJXx9A9fsVr25H6gESwSPYJ3kOe3BF4+4qNnQzZXMVr5tbgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('fe4ffd95d0f99b968144e632168904cc')
 
-line_bot_api.push_message('U9331f84776672cb357b3b8b9f89ebeaf', TextSendMessage(text='You can start !'))
+line_bot_api.push_message('U9331f84776672cb357b3b8b9f89ebeaf', TextSendMessage(text='You can start !')) # Put your ID
 
 
 @app.route("/callback", methods=['POST'])
@@ -24,3 +24,10 @@ def callback():
         abort(400)
  
     return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+
+if __name__ == '__main__':
+    app.run()
