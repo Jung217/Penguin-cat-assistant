c82345b2fa2b23dc61041bc03b6f460129f035d1
Jung217 <alex24922665@gmail.com>
2022-12-20 23:16:08 +0800

    Add Map NQUv4

--- Test/test.py
+++ Test/test.py
@@ -1,4 +1,6 @@
-import random
-
-bless = ['生日快樂！希望你的所有願望都能成真','準備好開始倒數了嗎？下次跟你說生日快樂是 365 天之後，生日快樂！','原本想送你一個最可愛的禮物，後來只能找第二可愛的，因為你排名第一呀。','大壽星，小蛋糕，絕配。生日快樂！','你生日的這一天，我沒有跟你在一起，只希望你能快樂、健康、美麗，生命需要奮鬥、創造和把握！生日快樂！','Happy birthday to the most wonderful friend in my heart.','Wish you a happy birthday! May the best and the loving things be some of the joy your birthday bring.']
-print(bless[random.randint(0, len(bless)-1)])
+    elif re.match("恭喜",message):
+        sticker_message = StickerSendMessage(
+            package_id='6325',
+            sticker_id='10979924'
+        )
+        line_bot_api.reply_message(event.reply_token, sticker_message)
