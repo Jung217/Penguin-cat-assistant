a585a205898c6e4f2dc582087066f29d7a3e5fad
Jung217 <alex24922665@gmail.com>
2022-12-24 15:28:23 +0800

    FM test v3

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-
+    #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    lineMessage = event.message.text
 
 if lineMessage[0:3]== "隨便吃" :
     address=""
