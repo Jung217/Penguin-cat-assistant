56fbdfaaeda75f2190c614c3cd059c4d3d0b5a21
Jung217 <alex24922665@gmail.com>
2023-01-02 00:29:59 +0800

    Pic DEAD

--- Test/test.py
+++ Test/test.py
@@ -9,3 +9,15 @@ WHurl = "https://penguin-cat-assistant.herokuapp.com"
             preview_image_url = WHurl + "/static/" + event.message.id + ".png"
         )
 		line_bot_api.reply_message(event.reply_token, image_message)
+
+
+    if event.message.type == "image":
+        SendImage = line_bot_api.get_message_content(event.message.id)
+
+		local_save = './Image/' + event.message.id + '.png'
+		with open(local_save, 'wb') as fd:
+			for chenk in SendImage.iter_content():
+				fd.write(chenk)
+
+		img_url = glucose_graph(d8f43d95eef9f03, local_save)
+		line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=img_url, preview_image_url=img_url))
--- app.py
+++ app.py
@@ -49,17 +49,6 @@ def handle_message(event):
         remessage = "觸發驚喜的密語:\n\n後製\n恭喜\n今天我生日\n金門大學在哪\n\n試著輸入看看吧!"
         line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
 
-    elif event.message.type == "image":
-        SendImage = line_bot_api.get_message_content(event.message.id)
-
-		local_save = './Image/' + event.message.id + '.png'
-		with open(local_save, 'wb') as fd:
-			for chenk in SendImage.iter_content():
-				fd.write(chenk)
-
-		img_url = glucose_graph(d8f43d95eef9f03, local_save)
-		line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=img_url, preview_image_url=img_url))
-
     elif "生日" in message:
         bless = ['生日快樂！希望你的所有願望都能成真','準備好開始倒數了嗎？下次跟你說生日快樂是 365 天之後，生日快樂！','原本想送你一個最可愛的禮物，後來只能找第二可愛的，因為你排名第一呀。','大壽星，小蛋糕，絕配。生日快樂！','你生日的這一天，我沒有跟你在一起，只希望你能快樂、健康、美麗，生命需要奮鬥、創造和把握！生日快樂！','Happy birthday to the most wonderful friend in my heart.','Wish you a happy birthday! May the best and the loving things be some of the joy your birthday bring.']
         line_bot_api.reply_message(event.reply_token,TextSendMessage(bless[random.randint(0, len(bless)-1)]))
