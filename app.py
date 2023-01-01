5754089e7ee75a2634813070959443d42dfd4b7f
Jung217 <alex24922665@gmail.com>
2023-01-01 20:59:06 +0800

    Final Aqua

--- app.py
+++ app.py
@@ -45,6 +45,39 @@ def handle_message(event):
     elif re.match("0",message):
         remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
         line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("1",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("2",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("3",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("4",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("5",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("6",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("7",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("8",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("9",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("10",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
+    elif re.match("11",message):
+        remessage = "https://astro.click108.com.tw/daily_"+ message + ".php?iAstro=" + message
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))
     elif "生日" in message:
         bless = ['生日快樂！希望你的所有願望都能成真','準備好開始倒數了嗎？下次跟你說生日快樂是 365 天之後，生日快樂！','原本想送你一個最可愛的禮物，後來只能找第二可愛的，因為你排名第一呀。','大壽星，小蛋糕，絕配。生日快樂！','你生日的這一天，我沒有跟你在一起，只希望你能快樂、健康、美麗，生命需要奮鬥、創造和把握！生日快樂！','Happy birthday to the most wonderful friend in my heart.','Wish you a happy birthday! May the best and the loving things be some of the joy your birthday bring.']
         line_bot_api.reply_message(event.reply_token,TextSendMessage(bless[random.randint(0, len(bless)-1)]))
