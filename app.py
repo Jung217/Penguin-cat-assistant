8cae875c8eb6f95f7b50797adce83a8401d59e02
Jung217 <alex24922665@gmail.com>
2023-01-02 14:37:53 +0800

    Add image v4

--- app.py
+++ app.py
@@ -114,17 +114,9 @@ def callback():
 
 @handler.add(MessageEvent)
 def handle_message(event):
-
-    if event.message.type == "image":
-		SendImage = line_bot_api.get_message_content(event.message.id)
-
-		local_save = './Image/' + event.message.id + '.png'
-		with open(local_save, 'wb') as fd:
-			for chenk in SendImage.iter_content():
-				fd.write(chenk)
-
-		img_url = glucose_graph('d8f43d95eef9f03', local_save)
-		line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=img_url, preview_image_url=img_url))
+    if (event.message.type == "image"):
+        SendImage = line_bot_api.get_message_content(event.message.id)
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(SendImage))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
