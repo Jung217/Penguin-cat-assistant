d97c98cce0e44984116e213c9417fd79b5aa2d05
Jung217 <alex24922665@gmail.com>
2023-01-02 21:55:48 +0800

    Add v V3

--- app.py
+++ app.py
@@ -208,15 +208,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=sendString))
 
     elif re.match("後製特效",message):
-        sendString = "https://colab.research.google.com/drive/1zuaIM3YK3jbfLykKOtk5fr8j6fLkT3Ce?authuser=2#scrollTo=TEBFojJ3hB27"
+        sendString = "https://colab.research.google.com/drive/1zuaIM3YK3jbfLykKOtk5fr8j6fLkT3Ce?authuser=2#scrollTo=TEBFojJ3hB27 \n https://youtu.be/Tp_tT4vJIdY"
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=sendString))
-
-    elif re.match('後製示範',message):
-        video_message = VideoSendMessage(
-            original_content_url='https://youtu.be/Tp_tT4vJIdY',
-            preview_image_url='https://imgur.com/9lDJ8nH'
-        )
-        line_bot_api.reply_message(event.reply_token, video_message)
 
     elif re.match("擲筊",message):
         sendString = "恭喜" + divinationBlocks() + "!"
@@ -239,9 +232,6 @@ def handle_message(event):
                         MessageAction( 
                             label="後製",
                             text="後製特效"),
-                        MessageAction( 
-                            label="示範",
-                            text="後製示範"),
                         MessageAction( 
                             label="更多",
                             text="更多特效"),
